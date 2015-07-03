89b98554f5b54df70fdb6799b0e38da6288fca53
GrandCircusPaul <paul@grandcircus.com>
2015-07-03 18:06:06 -0400

    Adding an Empty MVC Web App Project
    The Repository was created first, and now the VS asp.net templated
    project, with MVC option was created.  No changes were made to the
    project.  This is the initial commit of the project to the repository.

---
+++ MyMVCWebApp/MyMVCWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMVCWebApp.Startup))]
+namespace MyMVCWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
